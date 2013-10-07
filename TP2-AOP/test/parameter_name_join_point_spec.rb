c47930d6d185adbe9483eb9ab1f7259260787606
MarianoL <marianolazzaro@gmail.com>
2013-10-06 22:13:25 -0300

    pequenio cambio al test

--- TP2-AOP/test/parameter_name_join_point_spec.rb
+++ TP2-AOP/test/parameter_name_join_point_spec.rb
@@ -3,11 +3,17 @@ require_relative '../framework/parameter_name_join_point'
 
 describe 'Test Parameter Name' do
 
-  it 'debe encontrar simbolo en los parametros del metodo' do
+  it 'debe encontrar simbolos en los parametros del metodo' do
     metodo = ->(param1,param2=2) {}
     jp = ParameterNameJoinPoint.new(metodo)
-    true.should == (jp.tiene_este_parametro? 'param1').empty?.!
-    true.should == (jp.tiene_este_parametro? 'param2').empty?.!
+    false.should == jp.tiene_este_parametro?('param1').empty?
+    false.should == jp.tiene_este_parametro?('param2').empty?
+  end
+
+  it 'NO debe encontrar ESTE simbolo en los parametros del metodo' do
+    metodo = ->(param1,param2=2) {}
+    jp = ParameterNameJoinPoint.new(metodo)
+    true.should == jp.tiene_este_parametro?('param3').empty?
   end
 
 end
