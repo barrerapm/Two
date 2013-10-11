6c72400badf159c99e3fc304483511db02f60272
MarianoL <marianolazzaro@gmail.com>
2013-10-10 21:51:36 -0300

    cambie de nombre el test porque RubyMine me esta volviendo loco, ejecuta 2 files de test al mismo tiempo

---
+++ TP2-AOP/test/tests_pointcuts_compuestos_spec.rb
@@ -0,0 +1,72 @@
+require 'rspec'
+require_relative '../framework/point_cut'
+require_relative '../framework/conditions/condition'
+require_relative 'parameter_name_join_point_spec'
+require_relative 'parameter_type_join_point_spec'
+
+describe 'Tests de Pointcuts compuestos con subclases de JPs' do
+
+  attr_accessor :point_cut, :metodo
+
+  before(:each) do
+    @point_cut = PointCut.new
+  end
+
+  it 'jp6 AND jp6, orden :a :b' do
+    metodo = ->(a,b,c) {}
+    jp6 = ParameterNameJoinPoint.new(:a)
+    jp6_2 = ParameterNameJoinPoint.new(:b)
+    point_cut.conditions << ConditionAnd.new(jp6)
+    point_cut.conditions << ConditionAnd.new(jp6_2)
+    # pp point_cut.conditions
+    true.should == jp6.aplica?(nil, metodo)
+    true.should == jp6_2.aplica?(nil, metodo)
+    true.should == point_cut.aplica?(nil, metodo)
+  end
+
+  it 'jp6 AND jp6, orden :b :a' do
+    metodo = ->(b,a,c) {}
+    jp6 = ParameterNameJoinPoint.new(:a)
+    jp6_2 = ParameterNameJoinPoint.new(:b)
+    point_cut.conditions << ConditionAnd.new(jp6)
+    point_cut.conditions << ConditionAnd.new(jp6_2)
+    # pp point_cut.conditions
+    true.should == jp6.aplica?(nil, metodo)
+    true.should == jp6_2.aplica?(nil, metodo)
+    true.should == point_cut.aplica?(nil, metodo)
+  end
+
+  it 'jp6 AND jp6 probando :a :b :c' do
+    metodo = ->(a,b,c) {}
+    jp6 = ParameterNameJoinPoint.new(:a)
+    jp6_2 = ParameterNameJoinPoint.new(:b)
+    jp6_3 = ParameterNameJoinPoint.new(:c)
+    point_cut.conditions << ConditionAnd.new(jp6)
+    point_cut.conditions << ConditionAnd.new(jp6_2)
+    point_cut.conditions << ConditionAnd.new(jp6_3)
+    # pp point_cut.conditions
+    true.should == jp6.aplica?(nil, metodo)
+    true.should == jp6_2.aplica?(nil, metodo)
+    true.should == jp6_3.aplica?(nil, metodo)
+    true.should == point_cut.aplica?(nil, metodo)
+  end
+
+  it 'jp6 AND jp6 probando :a :b :c :d' do
+    metodo = ->(a,b,c) {}
+    jp6 = ParameterNameJoinPoint.new(:a)
+    jp6_2 = ParameterNameJoinPoint.new(:b)
+    jp6_3 = ParameterNameJoinPoint.new(:c)
+    jp6_4 = ParameterNameJoinPoint.new(:d)
+    point_cut.conditions << ConditionAnd.new(jp6)
+    point_cut.conditions << ConditionAnd.new(jp6_2)
+    point_cut.conditions << ConditionAnd.new(jp6_3)
+    point_cut.conditions << ConditionAnd.new(jp6_4)
+    # pp point_cut.conditions
+    true.should == jp6.aplica?(nil, metodo)
+    true.should == jp6_2.aplica?(nil, metodo)
+    true.should == jp6_3.aplica?(nil, metodo)
+    false.should == jp6_4.aplica?(nil, metodo)
+    false.should == point_cut.aplica?(nil, metodo)
+  end
+
+end
