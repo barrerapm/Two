1c17af7678fc04b315c4fa872f18952a9f12b3f7
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-22 15:16:56 -0200

    Corrección.

--- TP2-AOP/framework/join_points/class_method_name.rb
+++ TP2-AOP/framework/join_points/class_method_name.rb
@@ -1,9 +1,14 @@
 require_relative 'join_point'
 
 class Class_method_name < JoinPoint
+  attr_accessor :metodo_buscado
+
+  def initialize(metodo_a_buscar)
+    @metodo_buscado = metodo_a_buscar
+  end
 
   def match?(clase, metodo)
-    clase.instance_methods(false).any? {|unMetodo| unMetodo.to_s == metodo.to_s}
+    clase.instance_methods(false).any? {|unMetodo| unMetodo.to_s == @metodo_buscado.to_s}
   end
 
 end
