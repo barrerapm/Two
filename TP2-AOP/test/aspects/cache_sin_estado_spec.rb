1f95a7998be2fd95aff2d8f169a5577c43b6b828
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-22 15:19:21 -0200

    Corrección. Falta terminar.

--- TP2-AOP/test/aspects/cache_sin_estado_spec.rb
+++ TP2-AOP/test/aspects/cache_sin_estado_spec.rb
@@ -4,11 +4,13 @@ require_relative '../../aspects/cache_sin_estado'
 require_relative '../../framework/interceptors/interceptor_after'
 require_relative '../../framework/interceptors/interceptor_before'
 require_relative '../../framework/interceptors/interceptor_instead_of'
+require_relative '../../framework/join_points/class_method_name'
 
 describe 'Aspecto de cache sin estado' do
 
   class Persona
     attr_accessor :vitalidad
+
     def initialize(vida)
       @vitalidad = vida
     end
@@ -23,6 +25,8 @@ describe 'Aspecto de cache sin estado' do
     fw = FrameworkAOP.new
     aspect_cache_con_estado = CacheSinEstadoAspect.new
     aspect_cache_con_estado.interceptor = InterceptorInsteadOf.new
+    point_cut_simple = Class_method_name.new(:caminar)
+    aspect_cache_con_estado.point_cut = point_cut_simple
     fw.load_aspect(aspect_cache_con_estado)
 
     persona = Persona.new(100)
