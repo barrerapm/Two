9282e6e4eea4d00f3efbeccb5666844ae8be060c
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-22 15:20:12 -0200

    corrección. Falta terminar.-

--- TP2-AOP/aspects/cache_sin_estado.rb
+++ TP2-AOP/aspects/cache_sin_estado.rb
@@ -1,57 +1,51 @@
 require_relative '../framework/aspect'
 
+class InvocacionSinEstado
+  attr_accessor :metodo, :argumentos, :resultado
+
+  def initialize(contexto,resultado)
+    @metodo = contexto.method_origin
+    @argumentos = contexto.parameter_values
+    @resultado = resultado
+  end
+end
+
 class CacheSinEstadoAspect < Aspect
 
-  attr_accessor :array_invocaciones_cache_sin_estado
+  attr_accessor :array_invocaciones
 
   def initialize
-    @array_invocaciones_cache_sin_estado = []
+    @array_invocaciones = []
   end
 
-  def exec(context)
-    ejecutar_metodo(context)
-  end
-
-  def ejecutar_metodo(context)
-    metodo_en_cache = buscar_metodo_en_cache(context)
+  def exec(contexto)
+    metodo_en_cache = buscar_metodo_en_cache(contexto)
     if metodo_en_cache == nil
-      agregar_y_ejecutar_metodo_en_cache(context)
+      agregar_y_ejecutar_metodo_en_cache(contexto)
       puts 'Agregado a cache'
     else
-      puts 'Invocado desde cache ' + metodo_en_cache.metodo.to_s
+      puts 'Invocado desde cache '
       metodo_en_cache.resultado
     end
   end
 
-  def buscar_metodo_en_cache(ctx)
-    if not @array_invocaciones_cache_sin_estado.empty?
-      @array_invocaciones_cache_sin_estado.detect {
-          |inv| inv.metodo.to_s == ctx.method_origin.to_s &&
-            inv.argumentos.eql?(ctx.parameter_values)
+  def buscar_metodo_en_cache(contexto)
+    if not @array_invocaciones.empty?
+      @array_invocaciones.detect {
+          |invocacion| validar_invocacion_en_cache(invocacion, contexto)
       }
     end
   end
 
-  def agregar_y_ejecutar_metodo_en_cache(context)
-    resultado = context.object.send context.method_origin, *context.parameter_values.map {|param| param[1]}.to_set
-    @array_invocaciones_cache_sin_estado << InvocacionConEstado.new(context, resultado)
+  def validar_invocacion_en_cache(invocacion, contexto)
+    invocacion.metodo.to_s == contexto.method_origin.to_s &&
+        invocacion.argumentos.eql?(contexto.parameter_values)
+  end
+
+  def agregar_y_ejecutar_metodo_en_cache(contexto)
+    resultado = contexto.object.send contexto.method_origin, *contexto.parameter_values.map {|param| param[1]}.to_set
+    @array_invocaciones << InvocacionSinEstado.new(contexto, resultado)
     resultado
   end
-
-  class InvocacionConEstado
-    attr_accessor :metodo, :argumentos, :resultado, :atributos
-    def initialize(ctx,resultado)
-      @metodo = ctx.method_origin
-      @argumentos = ctx.parameter_values
-      @resultado = resultado
-    end
-  end
-
-  def methods_to_intercept(clase)
-    clase.instance_methods(false).keep_if do |method|
-      method.to_s == 'caminar'
-    end
-  end
-
 
 end
