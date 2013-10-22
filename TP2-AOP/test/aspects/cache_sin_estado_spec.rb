ccdd18a5ebf413ad3f7fea5095afc4988098c47c
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-21 20:58:42 -0500

    Commit por corte de luz =)

--- TP2-AOP/test/aspects/cache_sin_estado_spec.rb
+++ TP2-AOP/test/aspects/cache_sin_estado_spec.rb
@@ -1,14 +1,37 @@
 require 'rspec'
+require_relative '../../framework/framework_aop'
+require_relative '../../aspects/cache_sin_estado'
+require_relative '../../framework/interceptors/interceptor_after'
+require_relative '../../framework/interceptors/interceptor_before'
+require_relative '../../framework/interceptors/interceptor_instead_of'
 
 describe 'Aspecto de cache sin estado' do
 
-  #Pasos del test
-  #1- Crear una clase con un metodo con un parametro y un atributo, donde el metodo devuelva attr + param
-  #2- Attr = 0; Llamar al metodo con 5 => metodo devuelve 5
-  #3- Setear Attr = 8; Llamar al metodo con 5 => metodo devuelve 5 (la cache no tiene en cuenta el estado del obj)
+  class Persona
+    attr_accessor :vitalidad
+    def initialize(vida)
+      @vitalidad = vida
+    end
 
-  it 'should prueba del funcionamiento de la cache' do
-    true.should == false
+    def caminar(km)
+      @vitalidad += km
+      return @vitalidad
+    end
+  end
+
+  it 'should prueba del funcionamiento de la cache sin estado' do
+    fw = FrameworkAOP.new
+    aspect_cache_con_estado = CacheSinEstadoAspect.new
+    aspect_cache_con_estado.interceptor = InterceptorInsteadOf.new
+    fw.load_aspect(aspect_cache_con_estado)
+
+    persona = Persona.new(100)
+    persona.caminar(10)
+    persona.vitalidad.should == 110
+    persona.caminar(50)
+    persona.vitalidad.should == 160
+    persona.caminar(10)
+    persona.vitalidad.should == 160    #no incremetna dado que en Cache sin estado no se accede al estado del objeto
   end
 
 end
