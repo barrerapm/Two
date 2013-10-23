a2311fb907e449b1c302e51efcf0a07b8ad6c6df
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-23 11:12:47 -0500

    Moficaciòn de HardCodding (se pasa por argumento un JP/PJ) ;)

--- TP2-AOP/test/aspects/cache_con_estado_spec.rb
+++ TP2-AOP/test/aspects/cache_con_estado_spec.rb
@@ -1,9 +1,11 @@
 require 'rspec'
 require_relative '../../framework/framework_aop'
+require_relative '../../aspects/cache_sin_estado'
 require_relative '../../aspects/cache_con_estado'
 require_relative '../../framework/interceptors/interceptor_after'
 require_relative '../../framework/interceptors/interceptor_before'
 require_relative '../../framework/interceptors/interceptor_instead_of'
+require_relative '../../framework/join_points/class_method_name'
 
 describe 'Aspecto de cache con estado' do
 
@@ -23,6 +25,8 @@ describe 'Aspecto de cache con estado' do
     fw = FrameworkAOP.new
     aspect_cache_con_estado = CacheConEstadoAspect.new
     aspect_cache_con_estado.interceptor = InterceptorInsteadOf.new
+    point_cut_simple = Class_method_name.new(:nacimiento)
+    aspect_cache_con_estado.point_cut = point_cut_simple
     fw.load_aspect(aspect_cache_con_estado)
 
     persona = Persona.new
