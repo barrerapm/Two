1590cd609833f06a91c97ef8bf34fe51fe3e739c
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-24 21:10:08 -0200

    Funciona OK =)

--- TP2-AOP/framework/join_points/accessors_method_name.rb
+++ TP2-AOP/framework/join_points/accessors_method_name.rb
@@ -3,17 +3,18 @@ require_relative 'join_point'
 class AccessorsMethodName < JoinPoint
 
   def match?(clase, metodo)
-    objeto = clase.new
     clase.instance_methods(false).any? do |unMetodo|
-      objeto.instance_eval  do
-        puts unMetodo.to_s
-        if unMetodo.to_s.end_with?("=")
-          variable_string = "@#{unMetodo}" - "="
-        else
-           variable_string = "@#{unMetodo}"
-        end
-        defined?(variable_string.to_sym)
-      end
+       es_metodo_accessor(clase, unMetodo)
     end
   end
+end
+
+def es_metodo_accessor(clase, metodo)
+  objeto = clase.new
+  if metodo.to_s.end_with?("=")
+    cadena_variable = metodo.to_s[0, metodo.to_s.length - 1]
+  else
+    cadena_variable = metodo.to_s
+  end
+  objeto.instance_variable_defined?("@#{cadena_variable}")
 end
