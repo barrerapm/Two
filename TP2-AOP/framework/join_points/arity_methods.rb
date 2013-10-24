4ed773e4890164fd0f2477e2053fbd1f5d31679c
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-24 21:23:13 -0200

    Funciona OK =)

--- TP2-AOP/framework/join_points/arity_methods.rb
+++ TP2-AOP/framework/join_points/arity_methods.rb
@@ -8,20 +8,14 @@ class Arity_methods
     @rango = rango_dado
   end
 
-  def aridad(unMetodo)
-    bloque = proc {unMetodo.to_sym}
-    bloque.arity
+  def aridad(unaClase, unMetodo)
+     obj = unaClase.new
+    obj.method(unMetodo).arity
   end
 
   def match?(unaClase, unMetodo)
     unaClase.instance_methods(false).any? do
-      |metodo|  @rango.to_a.include?(aridad(metodo))
-    end
-  end
-
-  def imprimir (unaClase)
-    unaClase.instance_methods(false).each do
-      |metodo, *arg| puts '-' + metodo.to_s
+      |metodo|  @rango.to_a.include?(aridad(unaClase, metodo))
     end
   end
 
