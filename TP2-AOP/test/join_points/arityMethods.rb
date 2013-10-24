d40dd47aa10af3b0490e1abcd735256f8a8d327d
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-23 20:28:57 -0500

    test, falta terminar

---
+++ TP2-AOP/test/join_points/arityMethods.rb
@@ -0,0 +1,24 @@
+require 'rspec'
+require_relative '../../framework/join_points/join_point'
+require_relative '../../framework/join_points/arity_methods'
+require_relative '../../framework/join_points/method_regex'
+
+describe 'Testeo de filtrado de metodos por aridad' do
+
+  class Persona
+    def vivir_en(direccion, numero, piso)
+    end
+    def definir_masa(peso, altura)
+    end
+    def caminar
+    end
+  end
+
+  it 'Encuentra el metodo' do
+   detector_aridad = Arity_methods.new([0..2])
+   #detector_aridad.imprimir(Persona)
+    resultado = detector_aridad.match?(Persona, /hola a todos/)
+
+    resultado.should == true
+  end
+end
