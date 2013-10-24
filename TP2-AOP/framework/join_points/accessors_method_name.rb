35c3f5cd5e3f4c688cde846857260e59f506ed3e
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-23 21:22:38 -0500

    Corregir!!

--- TP2-AOP/framework/join_points/accessors_method_name.rb
+++ TP2-AOP/framework/join_points/accessors_method_name.rb
@@ -2,12 +2,18 @@ require_relative 'join_point'
 
 class AccessorsMethodName < JoinPoint
 
-  def tieneEstaVariableConEsteMetodo (unaClase, unMetodo)
-    unaClase.instance_variable_get(unMetodo)
+  def match?(clase, metodo)
+    objeto = clase.new
+    clase.instance_methods(false).any? do |unMetodo|
+      objeto.instance_eval  do
+        puts unMetodo.to_s
+        if unMetodo.to_s.end_with?("=")
+          variable_string = "@#{unMetodo}" - "="
+        else
+           variable_string = "@#{unMetodo}"
+        end
+        defined?(variable_string.to_sym)
+      end
+    end
   end
-
-  def aplica?(clase, metodo)
-    self.tieneEstaVariableConEsteMetodo(unaclase, unMetodo) || self.tieneEstaVariableConEsteMetodo(unaclase, unMetodo + '=')
-  end
-
 end
