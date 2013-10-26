c998f1194bd51b2322e0da8b349794207864236f
bunker668 <k091_debinski_leonel@yahoo.com.ar>
2013-10-25 19:55:30 -0500

    Breve corrección ;)

--- TP2-AOP/aspects/cache_sin_estado.rb
+++ TP2-AOP/aspects/cache_sin_estado.rb
@@ -24,6 +24,7 @@ class CacheSinEstadoAspect < Aspect
     if metodo_en_cache == nil
       agregar_y_ejecutar_metodo_en_cache(contexto)
     else
+      puts 'Invocado: ' + metodo_en_cache.metodo
       metodo_en_cache.resultado
     end
   end
@@ -45,6 +46,7 @@ class CacheSinEstadoAspect < Aspect
   def agregar_y_ejecutar_metodo_en_cache(contexto)
     resultado = contexto.object.send contexto.method_origin, *contexto.parameter_values.map {|param| param[1]}.to_set
     @array_invocaciones << @clase_de_invocacion.new(contexto, resultado)
+    puts 'Cacheado: ' + contexto.method_origin
     resultado
   end
 
