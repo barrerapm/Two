84f2bcde7a1158317b7b3c838553e3b0df93e4e2
MarianoL <marianolazzaro@gmail.com>
2013-11-17 18:32:28 -0300

    fixes2

--- TP2-AOP/alternativo/join_points/operaciones.rb
+++ TP2-AOP/alternativo/join_points/operaciones.rb
@@ -6,7 +6,6 @@ class Operaciones
 
   alias :& :and
 
-  # TODO: ver si se puede arreglar esto (se repite 1 linea de codigo, el fin del mundo)
   # no repetir codigo aca seria dificil, ya se intento con un metodo __operacion__, pero despues
   # el __method__ devuelve :__operacion__ en lugar de :and, y __callee__ devuelve :& si fue llamado
   # con &, asi que eso romperia el polimorfismo y el alias (elegir uno de 2: o el alias o el polimorfismo)
