958327ae8df38f131ba82ca49478debea45ad925
MarianoL <marianolazzaro@gmail.com>
2013-11-23 03:45:44 -0300

    arreglito

--- TP3/framework/join_points/accessors_method_name.rb
+++ TP3/framework/join_points/accessors_method_name.rb
@@ -2,13 +2,13 @@ require_relative 'join_point'
 
 class AccessorsMethodName < JoinPoint
 
-  def es_metodo_accessor(clase, metodo)
+  def es_metodo_accessor(metodo)
     metodo.to_s.end_with?("=")
   end
 
   def match?(clase, metodo)
     filtrados = clase.instance_methods(false)
-    filtrados.include?(metodo) && es_metodo_accessor(clase, metodo)
+    filtrados.include?(metodo) && es_metodo_accessor(metodo)
   end
 
 end
